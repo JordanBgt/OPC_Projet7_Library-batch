e6c6984787243614f73d2b076cbc04d9e0df5757
Jordan <jordanbouget@gmail.com>
2020-09-04 15:03:24 +0200

    Initial commit

---
+++ src/main/java/com/openclassrooms/librarybatch/LibraryBatchApplication.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.librarybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LibraryBatchApplication {
+
+	public static void main(String[] args) { SpringApplication.run(LibraryBatchApplication.class, args); }
+
+}
