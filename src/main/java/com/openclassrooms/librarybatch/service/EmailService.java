b9c5ba5ecbffbc099ab75ddca6ec7936aaf13064
Jordan <jordanbouget@gmail.com>
2020-09-04 15:53:04 +0200

    Modification mise en forme mail + modif horaire planification

--- src/main/java/com/openclassrooms/librarybatch/service/EmailService.java
+++ src/main/java/com/openclassrooms/librarybatch/service/EmailService.java
@@ -1,33 +1,34 @@
 package com.openclassrooms.librarybatch.service;
 
 import com.openclassrooms.librarybatch.model.Loan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Component
 public class EmailService {
-    Logger log = LoggerFactory.getLogger(EmailService.class);
 
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendReminderMail(Loan loan) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(loan.getUser().getEmail());
-        log.info("EMAIL : " + loan.getUser().getEmail());
-        message.setSubject("Les bibliothèques de Lyon : prêt échu");
-        message.setText("Bonjour " + loan.getUser().getUsername() + ", Nous avons le regret de vous informer que votre " +
+    public void sendReminderMail(Loan loan) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+        String htmlMessage = "<h5>Bonjour " + loan.getUser().getUsername() + ",</h5> <p>Nous avons le regret de vous informer que votre " +
                 "prêt pour le document " + loan.getExemplar().getDocumentTitle() + " (" +
                 loan.getExemplar().getReference() + ") emprunté dans une de nos bibliothèque (" +
                 loan.getExemplar().getLibrary().getName() + ") est arrivé à échéance depuis le " +
-                loan.getEndDate() + ". Nous vous prions de bien vouloir rendre ce document dans les plus brefs délais " +
+                loan.getEndDate() + ".</p> <p>Nous vous prions de bien vouloir rendre ce document dans les plus brefs délais " +
                 "afin de ne pas avoir à vous infliger des pénalités de retard ainsi qu'une interdiction " +
-                "temporaire d'emprunt de document dans toutes nos bibliothèques. Nous vous remercions et vous " +
-                "souhaitons une agrèable journée. Les bibliothèques de Lyon");
+                "temporaire d'emprunt de document dans toutes nos bibliothèques. </p> <p>Nous vous remercions et vous " +
+                "souhaitons une agrèable journée.</p> <br /><h5>Les bibliothèques de Lyon</h5>";
+        message.setContent(htmlMessage, "text/html");
+        helper.setTo(loan.getUser().getEmail());
+        helper.setSubject("Les bibliothèques de Lyon : prêt échu");
         emailSender.send(message);
     }
 
--- src/main/java/com/openclassrooms/librarybatch/task/SendMailTask.java
+++ src/main/java/com/openclassrooms/librarybatch/task/SendMailTask.java
@@ -3,18 +3,15 @@ package com.openclassrooms.librarybatch.task;
 import com.openclassrooms.librarybatch.model.Loan;
 import com.openclassrooms.librarybatch.proxy.LoanProxy;
 import com.openclassrooms.librarybatch.service.EmailService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 @Component
 public class SendMailTask {
-
-    Logger log = LoggerFactory.getLogger(SendMailTask.class);
 
     @Autowired
     private LoanProxy loanProxy;
@@ -22,13 +19,15 @@ public class SendMailTask {
     @Autowired
     private EmailService emailService;
 
-    @Scheduled(cron = "0 24 14 ? * * ", zone = "Europe/Paris")
+    @Scheduled(cron = "0 0 8 ? * * ", zone = "Europe/Paris")
     public void sendMail() {
-        log.info("TACHE LANCEE");
         List<Loan> loans = loanProxy.getAllEndedLoans();
-        log.info("PRET " + loans.get(0));
         for (Loan loan : loans) {
-            emailService.sendReminderMail(loan);
+            try {
+                emailService.sendReminderMail(loan);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
